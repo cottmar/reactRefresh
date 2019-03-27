145e90aef67c2131235f7620f3e37e60e824a526
cara ottmar <cara.ottmar@gmail.com>
2019-03-27 16:32:45 -0700

    saving end of day

--- react-refresh/src/containers/App.js
+++ react-refresh/src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import classes from './App.css';
+import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
   state = {
@@ -49,16 +49,9 @@ class App extends Component {
   }
 
   render() {
-    const style= {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
 
     let persons = null;
+    let btnClass = ' ';
 
     if (this.state.showPersons) {
       persons = (
@@ -74,32 +67,30 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red
     }
     // 'red bold'
     // let classes = ['red', 'bold'].join(' ');
     
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red];
+      assignedClasses.push( classes.red); // classes = ['red];
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold') // classes = ['red', 'bold'];
+      assignedClasses.push( classes.bold); // classes = ['red', 'bold'];
     }
 
     return (
-      <StyleRoot>
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a react app.</h1>
-        <p className={classes.join(' ')}>this is really working</p>
-        <button 
-          style={style}
+        <p className={assignedClasses.join(' ')}>this is really working</p>
+        <button
+          className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons
         </button> 
         {/* // reference to either render nothing or persons */}
         {persons}
       </div>
-    </StyleRoot>
     );
   }
 }

bf58afefeea7df67f4d8901e9cd13ca3b0f8c8a4
cara ottmar <cara.ottmar@gmail.com>
2019-04-01 13:12:05 -0700

    saving to finish

--- react-refresh/src/components/Cockpit/Cockpit.js
+++ react-refresh/src/components/Cockpit/Cockpit.js
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import classes from './Cockpit.css';
+import AuthContext from '../../context/auth-context';
 
 
 const cockpit = (props) => {
+  const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
+
+  console.log(authContext.authenticated);
+
+
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
     // http request...
     // want to control when it executes
-    setTimeout(() => {
-        // alert('Saved data to cloud!');
-    }, 1000);
+    // setTimeout(() => {
+    //     // alert('Saved data to cloud!');
+    // }, 1000);
+    toggleBtnRef.current.click();
     return () => {
       console.log('[Cockpit.js] cleanup work in useEffect')
     }
@@ -47,9 +55,12 @@ const cockpit = (props) => {
       </h1>
       <p className={assignedClasses.join(' ')}>this is really working</p>
       <button
+        ref={toggleBtnRef}
         className={btnClass}
         onClick={props.clicked}>Toggle Persons
       </button> 
+      <button onClick={authContext.login}>Log in</button>
+      
   </div>
   );
 }
--- react-refresh/src/components/Persons/Person/Person.js
+++ react-refresh/src/components/Persons/Person/Person.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import classes from './Person.css';
-import withClass from '../../../hoc/withClass';
+import withClass from '../../../hoc/WithClass';
+import PropTypes from 'prop-types';
+import AuthContext from '../../../context/auth-context';
 
 // Fragment works the same as the Aux component -- gets rid of needing a div
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+// static means it can be accessed from outside without the need to instantiate a class first
+// this allows react to connect to your context behind the scenes and gives you a new context property  
+static contextType = AuthContext;
+
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
+  }
+
+  // componentDidMount() {
+  //   this.inputElement.focus();
+  // }
+
   render() {
     console.log('[Person.js] rendering...');
 
     return (
-    <Aux>
+    <Aux> 
+    {this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
     <p onClick={this.props.click}>
       I'm {this.props.name} and I am {this.props.age} years old
     </p>
     <p key="i2">{this.props.children}</p>
     <input
       key="i3"
+      // ref={(inputEl) => {this.inputElement = inputEl}}
+      ref={this.inputElementRef}
       type="text" 
       onChange={this.props.changed} 
       value={this.props.name}
@@ -25,11 +47,15 @@ class Person extends Component {
     );
   }
 }
-
+// react will look out for this and make sure data is passed in correctly
 Person.propTypes = {
+  // for a click, i expect a pointer at a function
   click: PropTypes.func,
+  //expects a string
   name: PropTypes.string,
+  // expects a number, esp important in calculations
   age: PropTypes.number,
+  // expects to point to a function
   changed: PropTypes.func
 };
 
